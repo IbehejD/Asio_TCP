5acce7b351e13cf0bab357b27e601615ff184f0d
IbehejD <217250@vutbr.cz>
2026-07-09 19:19:14 +0000

    remake conan structure

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf ./build 
-conan install .conan --output-folder=build --build=missing -pr:h=.conan/profiles/default -pr:b=.conan/profiles/default -s build_type=Debug
+conan install . --output-folder=build --build=missing -pr:h=.conan/profiles/default -pr:b=.conan/profiles/default -s build_type=Debug
 cmake --preset conan-debug
 cmake --build --preset conan-debug
