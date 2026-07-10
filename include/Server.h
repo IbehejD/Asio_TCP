563d3568fb8e397179129f6ad349fe181644142a
IbehejD <217250@vutbr.cz>
2026-07-10 05:54:20 +0000

    refactor server to coroutine

--- include/Server.h
+++ include/Server.h
@@ -3,26 +3,14 @@
 
 #include <asio.hpp>
 
-class ServerConnection : public std::enable_shared_from_this<ServerConnection>
-{
-public:
-    ServerConnection(asio::io_context &io_context);
-    void write(asio::error_code error);
-    void handle_write(std::shared_ptr<std::string> msg, asio::error_code error);
-    asio::ip::tcp::socket &getSocket();
-
-private:
-    asio::ip::tcp::socket socket_;
-};
-
 class Server
 {
 public:
     Server(asio::io_context &io_context);
-    void start_accept();
 
 private:
-    void handle_accept(std::shared_ptr<ServerConnection>, asio::error_code error);
+    asio::awaitable<void> start_accept();
+    asio::awaitable<void> session(asio::ip::tcp::socket socket);
     asio::ip::tcp::acceptor acceptor_;
     asio::io_context &io_context_;
 };
