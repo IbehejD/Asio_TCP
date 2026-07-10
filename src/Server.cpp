2040f86164d8255daf168b2fa32cc6f2789a9a3e
IbehejD <217250@vutbr.cz>
2026-07-10 05:54:20 +0000

    refactor server to coroutine

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,36 +2,22 @@
 
 using asio::ip::tcp;
 
-ServerConnection::ServerConnection(asio::io_context &io_context) : socket_(io_context) {};
-
-tcp::socket &ServerConnection::getSocket()
-{
-    return socket_;
-};
-
-void ServerConnection::write(asio::error_code error)
-{
-    std::shared_ptr<std::string> msg = std::make_shared<std::string>("Hello world!\n");
-    asio::async_write(socket_, asio::buffer(*msg), std::bind(&ServerConnection::handle_write, shared_from_this(), msg, asio::placeholders::error));
-};
-
-void ServerConnection::handle_write(std::shared_ptr<std::string> msg, asio::error_code error) {
-
-};
-
-void Server::handle_accept(std::shared_ptr<ServerConnection> connection, asio::error_code error)
-{
-    connection->write(error);
-    start_accept();
-};
-
 Server::Server(asio::io_context &io_context) : io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
 {
-    start_accept();
+    co_spawn(io_context_, start_accept(), asio::detached);
 }
 
-void Server::start_accept()
+asio::awaitable<void> Server::session(tcp::socket socket)
 {
-    std::shared_ptr<ServerConnection> connection = std::make_shared<ServerConnection>(io_context_);
-    acceptor_.async_accept(connection->getSocket(), std::bind(&Server::handle_accept, this, connection, asio::placeholders::error));
+    std::string msg = "Hello world!\n";
+    co_await asio::async_write(socket, asio::buffer(msg), asio::use_awaitable);
 };
+
+asio::awaitable<void> Server::start_accept()
+{
+    for (;;)
+    {
+        tcp::socket socket = co_await acceptor_.async_accept(asio::use_awaitable);
+        co_spawn(io_context_, session(std::move(socket)), asio::detached);
+    };
+}
